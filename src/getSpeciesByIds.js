38accb7c82e71c8b9e0d9336761f1336fe656a6c
beto-ouverney <beto.paz30@gmail.com>
2022-01-25 16:07:04 -0300

    Requisito 1 OK

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,14 +1,9 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
+function getSpeciesByIds(...ids) {
   // seu código aqui
-  if (ids.length === 0) {
-    return [];
-  }
-  const filterSpeciesByid = (id) => data.species.id === id;
-  if (ids.length === 1) {
-    return data.find(filterSpeciesByid(ids));
-  }
+  console.log(...ids);
+  const array = data.species;
+  return array.filter((element) => ids.includes(element.id));
 }
-
 module.exports = getSpeciesByIds;
