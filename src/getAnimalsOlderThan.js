0e22bb84b1dff727280754a891458beda0b84347
beto-ouverney <beto.paz30@gmail.com>
2022-01-25 17:13:08 -0300

    Requisito 2 Ok
    Uso do find e every

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -2,6 +2,9 @@ const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
   // seu código aqui
+  const array = data.species;
+  const animalResidents = array.find((element) => element.name === animal).residents;
+  return animalResidents.every((resident)=> resident.age >= age);
 }
 
 module.exports = getAnimalsOlderThan;
