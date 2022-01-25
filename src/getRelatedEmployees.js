f8b84ef11a30104136c5345c21e6641166d7ef4f
beto-ouverney <beto.paz30@gmail.com>
2022-01-25 18:13:06 -0300

    Quesito 3 Ok

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,20 @@
 const data = require('../data/zoo_data');
 
+const array = data.employees;
+
 function isManager(id) {
   // seu código aqui
+  const filterEmployees = (employee) => employee.managers.includes(id);
+  return array.some(filterEmployees);
 }
 
 function getRelatedEmployees(managerId) {
   // seu código aqui
+  if (isManager(managerId)) {
+    const filterEmployees = (employee) => employee.managers.includes(managerId);
+    return array.filter(filterEmployees);
+  }
+  throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
 }
 
 module.exports = { isManager, getRelatedEmployees };
