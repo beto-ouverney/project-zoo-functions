58266e8d6c0568e92d4530f90dc240f37acf89d2
beto-ouverney <beto.paz30@gmail.com>
2022-01-27 16:57:33 -0300

    Requisito 8 - OK
    Sem as novas elaborações ainda...

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,63 @@
 const data = require('../data/zoo_data');
 
+const { species } = data;
+
+const { hours } = data;
+
+const daysNames = Object.keys(hours);
+
+const mondayObj = {
+  Monday: { officeHour: 'CLOSED', exhibition: 'The zoo will be closed!' },
+
+};
+
+const animals = species.map((animal) => animal.name);
+
+const arrayOfficeHour = (param) => {
+  const oHour = param.map((day) => (
+    `Open from ${hours[day].open}am until ${hours[day].close}pm`));
+  return oHour;
+};
+
+const days = (param) => param.reduce((acum, day, ind) =>
+  (
+    {
+      ...acum,
+      [param[ind]]: {
+        officeHour: arrayOfficeHour(param)[ind],
+        exhibition: species.filter((a) => a.availability.includes(param[ind])).map((e) => e.name),
+      },
+    }), {});
+
+const getDefaultSchedule = (param) => {
+  console.log(param);
+  const schedule = days(param);
+  if (param.includes('Monday')) {
+    schedule.Monday.officeHour = 'CLOSED';
+    schedule.Monday.exhibition = 'The zoo will be closed!';
+  }
+  return schedule;
+};
 function getSchedule(scheduleTarget) {
-  // seu código aqui
+  if (scheduleTarget === 'Monday') {
+    return mondayObj;
+  }
+  if (daysNames.includes(scheduleTarget)) {
+    const param = [];
+    param.push(scheduleTarget);
+    const result = getDefaultSchedule(param);
+    return result;
+  }
+  if (animals.includes(scheduleTarget)) {
+    const result = species.find((animal) => animal.name === scheduleTarget);
+    return result.availability;
+  }
+
+  const schedule = getDefaultSchedule(daysNames);
+
+  return schedule;
 }
+
+console.log(getSchedule('lions'));
 
 module.exports = getSchedule;
